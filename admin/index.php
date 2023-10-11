a3cf5ee2f007a1f72f15721f9f3254d7eeceb3e1
Warapornf12046 <fin3622@gmail.com>
2023-10-11 13:06:40 +0700

    indexphp

---
+++ admin/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>FOOD ORDER -HOME</title>
+      <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+      
+      <div class="menu">
+            <div class="wrapper">
+                  <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Admin</a></li>
+                        <li><a href="#">Category</a></li>
+                        <li><a href="#">Food</a></li>
+                        <li><a href="#">Order</a></li>
+                  </ul>
+                 
+            </div>
+            
+
+      </div>
+      
+      <div class="main-content">
+            <div class="wrapper">
+                  mid
+            </div>
+    
+      </div>
+      <div class="footer">
+            <div class="wrapper">
+                  <p class="text-center">foot food order- <a href="#">team</a>
+            </p>  
+            </div>
+            
+      </div>
+</body>
+</html>
